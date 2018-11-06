9f38bd7a274d88d0158ecb1948d9b5255b341eb4
Samuel Tonini <samueltonini@msn.com>
2018-11-06 21:28:33 -0200

    Fix GitHub Page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
+
+serviceWorker.unregister();
