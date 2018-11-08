bd316b8634eaea8323812cd128d31161594b976f
Samuel Tonini <samueltonini@msn.com>
2018-11-07 22:45:36 -0200

    Updates

---
+++ precache-manifest.30a7581f1a784ed4fd630cbdffadf467.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "4299e6c86880391c4ddd",
+    "url": "/my-reads/static/js/main.4299e6c8.chunk.js"
+  },
+  {
+    "revision": "4df072975aa9a1b21478",
+    "url": "/my-reads/static/js/1.4df07297.chunk.js"
+  },
+  {
+    "revision": "fdd3c7e0945ff9c19b1d343f3685a42f",
+    "url": "/my-reads/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/my-reads/precache-manifest.aa759881cb6d310efe287b2ee9a22adc.js"
+  "/my-reads/precache-manifest.30a7581f1a784ed4fd630cbdffadf467.js"
 );
 
 workbox.clientsClaim();
